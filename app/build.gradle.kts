e88e6bab0ba52bd24357ace4d8b1e29fb88330ef
Samuel Marquez Ruiz <samuelmarquez588@gmail.com>
2025-10-07 14:39:03 +0200

    Nav delete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -41,7 +40,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
